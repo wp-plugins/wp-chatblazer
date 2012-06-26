f857ac51791b1aa95d70df7403b18e89370f3d8f
tlecoultre <tlecoultre@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-26 03:20:57 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-chatblazer/trunk@563867 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-chatblazer.php
+++ wp-chatblazer.php
@@ -5,7 +5,7 @@
 Plugin Name: WP-Chatblazer
 Plugin URI: http://www.wp-chatblazer.com
 Description: Installs a Chatblazer chat application in a Wordpress site
-Version: 1.1
+Version: 1.4
 Date:  June 25, 2012
 Author: Phyllis Erck
 Author URI: http://www.phylliserck.net
@@ -34,6 +34,7 @@ License: GPL2
 /* Release History :
  * 1.0:       Initial release
  * 1.1:		  Added description, tags, and link in settings page to chatblazer	
+ * 1.2:	      Fixed link
 */
 
 /*****************
